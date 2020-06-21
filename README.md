450a7cc270e8b44f5ba70d9d9dae83377a8c9f20
Brian Xu <xu.brian.s@gmail.com>
2020-06-21 01:04:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# torch_converter
+
+Utilities for converting PyTorch models saved in Python 2 to models compatible with Python 3.
+
+There aren't many PyTorch 2 models available online and the format they were pickled in can vary greatly.
+As a result, the functions are defined individually rather than as a single function to convert a model.
+
+# Example Usage
+
+```python
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=1):
+        super(AlexNet, self).__init__()
+        ...
+```
+
+```python
+model = torch2to3.load_model('./models/alexnet.pth')
+model.keys() # dict_keys(['optimizer', 'epoch', 'state_dict', 'best_prec1'])
+state_dict = torch2to3.byte_convert(model['state_dict'])
+torch2to3.save_model(AlexNet, state_dit)
+```
---
+++ torch2to3.py
@@ -0,0 +1,30 @@
+import torch
+from torch import nn
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+def byte_convert(model: dict) -> dict:
+    new_model = dict()
+    for key in model:
+        if type(key) == bytes:
+            new_key = key.decode("utf-8")
+        else:
+            new_key = key
+        new_model[new_key] = dict()
+        if isinstance(model[key], dict):
+            new_model[new_key] = byte_convert(model[key])
+        else:
+            new_model[new_key] = model[key]
+    return new_model
+
+
+def load_model(model_path: str) -> dict:
+    return torch.load(model_path, encoding='bytes', map_location=device)
+
+
+def save_model(architecture: nn.Module, state_dict: dict) -> None:
+    model = architecture().to(device)
+    model.load_state_dict(state_dict)
+    model.eval()
+    torch.save(model, architecture.__name__ + '.pth')
